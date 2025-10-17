5b5de04d7e4482ede9d95c7bbfe128f15291a600
joaogabsoaresf <jgsoares.francisco@gmail.com>
2025-10-17 18:37:50 -0300

    fix: fix project service validation

--- src/main/java/dev/matheuslf/desafio/inscritos/service/ProjectService.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/service/ProjectService.java
@@ -2,17 +2,24 @@ package dev.matheuslf.desafio.inscritos.service;
 
 import dev.matheuslf.desafio.inscritos.entity.Project;
 import dev.matheuslf.desafio.inscritos.repository.ProjectRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final Validator validator;
 
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, Validator validator) {
         this.projectRepository = projectRepository;
+        this.validator = validator;
     }
 
     public List<Project> findAll() {
@@ -20,9 +27,18 @@ public class ProjectService {
     }
 
     public Project save(Project project) {
+        Set<ConstraintViolation<Project>> violations = validator.validate(project);
+        if (!violations.isEmpty()) {
+            throw new IllegalArgumentException(
+                    violations.stream()
+                            .map(ConstraintViolation::getMessage)
+                            .collect(Collectors.joining(", "))
+            );
+        }
         if (project.getStartDate() == null) {
             project.setStartDate(LocalDateTime.now());
         }
+
         return projectRepository.save(project);
     }
 }
