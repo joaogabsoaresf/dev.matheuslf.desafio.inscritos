d111b445d7d7f93f9c5a3b7ebebbbeba36b1e8ff
joaogabsoaresf <jgsoares.francisco@gmail.com>
2025-10-17 17:48:36 -0300

    fix: add lombok to project entity

--- src/main/java/dev/matheuslf/desafio/inscritos/entity/Project.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/entity/Project.java
@@ -1,5 +1,8 @@
 package dev.matheuslf.desafio.inscritos.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -9,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "projects")
 public class Project extends BaseEntity {
 
@@ -21,9 +26,11 @@ public class Project extends BaseEntity {
     private String description;
 
     @Column(name = "start_date", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDate;
 
     @Column(name = "end_date")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endDate;
 
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
