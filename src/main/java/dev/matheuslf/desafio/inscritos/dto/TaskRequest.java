b457a9132292be96c90e815fd98942a685b90d5a
joaogabsoaresf <jgsoares.francisco@gmail.com>
2025-10-18 18:21:29 -0300

    feat: add task request validators

--- src/main/java/dev/matheuslf/desafio/inscritos/dto/TaskRequest.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/dto/TaskRequest.java
@@ -4,12 +4,22 @@ import java.time.LocalDate;
 import java.util.UUID;
 import dev.matheuslf.desafio.inscritos.enums.TaskPriority;
 import dev.matheuslf.desafio.inscritos.enums.TaskStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record TaskRequest(
+        @NotNull(message = "O titulo da task é obrigatorio")
+        @Size(min = 5, max = 150)
         String title,
+
         String description,
+
         TaskPriority priority,
+
         TaskStatus status,
+
         LocalDate dueDate,
+
+        @NotNull(message = "O ID do projeto é obrigatório")
         UUID projectId
 ) {}
